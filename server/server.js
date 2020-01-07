f5ce9cb85e4a62deab8d8586bb24e5be69e48f57
Emmanuel Correa <emmanuel.correa@espol.cl>
2020-01-07 18:38:57 -0300

    configuraciones importantes

--- server/server.js
+++ server/server.js
@@ -8,46 +8,28 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
     // parse application/json
 app.use(bodyParser.json())
+app.use(require('./routes/usuario'))
 
 require('./config/config')
-app.get('/', function(req, res) {
-    res.json('Hello World')
-})
-app.get('/usuario', function(req, res) {
-    res.json('GET usarios')
-})
-app.post('/usuario', function(req, res) {
 
-    let body = req.body
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario',
-            error: 'Error 400, BAD REQUEST'
-        })
-    } else {
+// app.get('/', function(req, res) {
+//     res.json('Hello World')
+// })
 
-        res.json({ persona: body })
-    }
-})
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id
-    res.json({
-        id
-    })
-})
-app.delete('/usuario', function(req, res) {
-    res.json('DELETE usarios')
-})
 
 
 //Forma de conexionde mongoDB en forma local)(hay qeu definir el puerto)
-//mongoose.connect('mongodb://localhost:(puerto por definir)/my_database', (err, res) => {
-mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
-    if (err) throw err;
-    console.log('Base de Datos ONLINE!!!');
+//mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+    (err, res) => {
+        if (err) {
+            console.log("ERROR AL CONECTAR");
+            throw err;
+        }
+        console.log('BBDD Online');
 
-})
+
+    })
 
 
 let port = process.env.PORT
