5ba54428136e4aeda8e1864d107dd761de8124e2
Emmanuel Correa <emmanuel.correa@espol.cl>
2020-01-15 09:05:52 -0300

    variable de entorno personalizada de heroku

--- server/config/config.js
+++ server/config/config.js
@@ -35,7 +35,7 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://strider:XcmJGFY5a4GW9PTk@cluster0-mzcgx.mongodb.net/cafe?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI
 }
 process.env.URLDB = urlDB;
 
